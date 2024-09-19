1ce983c4911bff1183050fd5e82495b23ab53fbb
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-09-19 13:25:26 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -10,7 +10,15 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
-await conn.sendMessage(from, { audio: fs.readFileSync('../Voice/alive.mp3') , ptt: true  , mimetype: 'audio/mpeg'}, { quoted: mek })
+let des = `👋 𝙷𝚎𝚕𝚕𝚘 ${pushname} 𝙸'𝚖 𝚊𝚕𝚒𝚟𝚎 𝚗𝚘𝚠
+
+*Im DARK-KENZO-MD Whatsapp Bot Create By Senesh🍂✨*
+
+I am dark kenzo md whatsapp bot. How can I help you.
+To get the menu, type as menu. If you need to know something about the bot,
+type as owner and direct the question to me. Good day
+
+ ©𝙳𝙰𝚁𝙺 𝙺𝙴𝙽𝚉𝙾 𝙼𝙳`
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
