ace308e5c20684b567ffac0964ec4fcd320bb2b7
KEVIN-LEVIN-TECH <kevinathenlevin@gmail.com>
2024-09-20 17:52:47 +0530

    Update alive.js

--- plugins/alive.js
+++ plugins/alive.js
@@ -14,11 +14,15 @@ let des = `👋 *𝙷𝚎𝚕𝚕𝚘 ${pushname} 𝙸'𝚖 𝚊𝚕𝚒𝚟𝚎
 
 *Im DARK-KENZO-MD Whatsapp Bot Create By Senesh🍂✨*
 
-*I am dark kenzo md whatsapp bot. How can I help you.
-To get the menu, type as menu. If you need to know something about the bot,
-type as owner and direct the question to me. Good day*
+*I am dark kenzo md whatsapp bot. How can I help you.*
+*To get the menu, type as menu.* *If you need to know something about the bot,*
+*type as owner and direct the question to me. Good day*
 
- ©𝙳𝙰𝚁𝙺 𝙺𝙴𝙽𝚉𝙾 𝙼𝙳`
+*My Whatsapp Chanel = https://whatsapp.com/channel/0029VakzRXO6rsQw95N67f0v*
+
+*Owner Number = +94773673969*
+
+©𝙳𝙰𝚁𝙺 𝙺𝙴𝙽𝚉𝙾 𝙼𝙳`
     
 return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: des},{quoted: mek})
 }catch(e){
